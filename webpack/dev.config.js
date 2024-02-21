4ac570cfe519b473d430a21c466e4dc591f02971
Ondřej Nejedlý <jsmesami@gmail.com>
2024-02-21 21:37:18 +0100

    Update Webpack Dev Server

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -6,9 +6,12 @@ module.exports = merge(commonConfig, {
   devtool: 'inline-source-map',
   devServer: {
     port: 3000,
-    proxy: {
-      '/': 'http://0.0.0.0:8000',
-    },
+    proxy: [
+        {
+            context: ['/'],
+            target: 'http://0.0.0.0:8000',
+        },
+    ],
     // We need hot=false (Disable HMR) to set liveReload=true
     hot: false,
     liveReload: true,
