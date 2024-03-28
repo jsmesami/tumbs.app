e28c02008fc052a6ac5a7a2c283a6e47238503c5
Ondřej Nejedlý <jsmesami@gmail.com>
2024-03-28 12:47:42 +0100

    Hash IDs of current page in query parameters

--- tumbs/static/js/cms/components/CurrentPageProvider.js
+++ tumbs/static/js/cms/components/CurrentPageProvider.js
@@ -1,23 +1,20 @@
-import React, { createContext, useEffect } from "react";
+import React, { createContext } from "react";
 import { useSearchParams } from "react-router-dom";
+import Hashids from "hashids";
 
+const hashids = new Hashids("CurrentPage", 5);
 export const PageContext = createContext(null);
 
 const CurrentPageProvider = ({ website, children }) => {
   const [searchParams, setSearchParams] = useSearchParams();
   const findPage = (id) => website.pages.find((pg) => pg.id === id);
-  const pageId = parseInt(searchParams.get("page"));
-  const currentPage = findPage(pageId) || website?.pages[0];
-
-  useEffect(() => {
-    setCurrentPage(currentPage?.id);
-  }, []);
+  const pageId = hashids.decode(searchParams.get("page"));
+  const currentPage = findPage(parseInt(pageId)) || website?.pages[0];
 
   const setCurrentPage = (id) => {
     const pg = findPage(parseInt(id));
-
     if (pg) {
-      setSearchParams({ page: pg.id });
+      setSearchParams({ ...searchParams, ...{ page: hashids.encode(pg.id) } });
     }
   };
 
